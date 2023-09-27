515f363decc58a2f144cb61608d46bd07598ff1d
surendra <surendra.chakali@hyniva.com>
2023-09-27 12:37:22 +0530

    new update employee

--- api.js
+++ api.js
@@ -9,126 +9,84 @@ const manager="Manger";
 const hr="hr";
 const employee="employee";
 
+const updateEmployeeDetail = async (event) => {
+  const response = { statusCode: 200 };
+  try {
+      const body = JSON.parse(event.body);
+      const fieldsToUpdate = [
+          'jobTitle',
+          'totalExperience',
+          'employmentStartDateInHyniva',
+          'supervisorName',
+          'projectName',
+          'clientName',
+          'plannedJoiningDateInTheProject',
+          'plannedEndDateInTheProject',
+          'actualJoiningDateInTheProject',
+          'actualEndDateInTheProject',
+          'location',
+          'billability',
+          'remarksOnBillability',
+          'laptopSpecification',
+          'asset1',
+          'asset2',
+      ];
 
-
-
-
-// const updateEmployeeDetail = async (event) => {
-//   const response = { statusCode: 200 };
-//   try {
-//       const body = JSON.parse(event.body);
-//       const fieldsToUpdate = [
-//           'jobTitle',
-//           'totalExperience',
-//           'employmentStartDateInHyniva',
-//           'supervisorName',
-//           'projectName',
-//           'clientName',
-//           'plannedJoiningDateInTheProject',
-//           'plannedEndDateInTheProject',
-//           'actualJoiningDateInTheProject',
-//           'actualEndDateInTheProject',
-//           'location',
-//           'billability',
-//           'remarksOnBillability',
-//           'laptopSpecification',
-//           'asset1',
-//           'asset2',
-//       ];
-
-//       // Filter the body object to include only the specified fields
+      // Filter the body object to include only the specified fields
      
-//       const updatedFields = {};
-//       for (const field of fieldsToUpdate) {
-//           if (body[field]) {
-//               updatedFields[field] = body[field];
-//           }
-//       }
+      const updatedFields = {};
+      for (const field of fieldsToUpdate) {
+          if (body[field]) {
+              updatedFields[field] = body[field];
+          }
+      }
 
-//       // Check if any fields to update were provided
-//       if (Object.keys(updatedFields).length === 0) {
-//           throw new Error('No fields to update specified.');
-//       }
+      // Check if any fields to update were provided
+      if (Object.keys(updatedFields).length === 0) {
+          throw new Error('No fields to update specified.');
+      }
 
-//       const objKeys = Object.keys(updatedFields);
-//       const params = {
-//           TableName: process.env.DYNAMODB_TABLE_NAME,
-//           Key: marshall({ eId: event.pathParameters.eId }),
-//           UpdateExpression: `SET ${objKeys
-//               .map((_, index) => `#key${index} = :value${index}`)
-//               .join(', ')}`,
-//           ExpressionAttributeNames: objKeys.reduce(
-//               (acc, key, index) => ({
-//                   ...acc,
-//                   [`#key${index}`]: key,
-//               }),
-//               {}
-//           ),
-//           ExpressionAttributeValues: marshall(
-//               objKeys.reduce(
-//                   (acc, key, index) => ({
-//                       ...acc,
-//                       [`:value${index}`]: updatedFields[key],
-//                   }),
-//                   {}
-//               )
-//           ),
-//       };
+      const objKeys = Object.keys(updatedFields);
+      const params = {
+          TableName: process.env.DYNAMODB_TABLE_NAME,
+          Key: marshall({ eId: event.pathParameters.eId }),
+          UpdateExpression: `SET ${objKeys
+              .map((_, index) => `#key${index} = :value${index}`)
+              .join(', ')}`,
+          ExpressionAttributeNames: objKeys.reduce(
+              (acc, key, index) => ({
+                  ...acc,
+                  [`#key${index}`]: key,
+              }),
+              {}
+          ),
+          ExpressionAttributeValues: marshall(
+              objKeys.reduce(
+                  (acc, key, index) => ({
+                      ...acc,
+                      [`:value${index}`]: updatedFields[key],
+                  }),
+                  {}
+              )
+          ),
+      };
 
-//       const updateResult = await client.send(new UpdateItemCommand(params));
-//       response.body = JSON.stringify({
-//           message: 'Successfully updated employee details.',
-//           updateResult,
-//       });
-//   } catch (e) {
-//       console.error(e);
-//       response.statusCode = 500;
-//       response.body = JSON.stringify({
-//           message: 'Failed to update employee details.',
-//           errorMsg: e.message,
-//           errorStack: e.stack,
-//       });
-//   }
-//   return response;
-// };
-
-
-
-
-const updateEmployeeDetail = async (event, context) => {
-    const response = { statusCode: 200 };
-    const body = JSON.parse(event.body)
-    await client.update({
-         //TableName: 'usersDB',
-         TableName: process.env.DYNAMODB_TABLE_NAME,
-         Key:{
-            eId: event.pathParameters.eId,
-         },
-         UpdateExpression: 'set totalExperience = :totalExperience, employmentStartDateInHyniva = :employmentStartDateInHyniva, supervisorName = :supervisorName, projectName = :projectName',
-
-         ExpressionAttributeValues:{
-            ':jobTitle': body.jobTitle,
-            ':totalExperience': body.totalExperience,
-            ':employmentStartDateInHyniva': body.employmentStartDateInHyniva,
-            ':supervisorName': body.supervisorName,
-            ':projectName': body.projectName,
-         },
-         ReturnValues: 'UPDATED_NEW',
-     }).promise()
-     response = {
-             'statusCode': 200,
-             'body': JSON.stringify({
-                 message: 'User is Updated',
-             })
-         }
-     return response
- };
-
-
-
-
-
-
+      const updateResult = await client.send(new UpdateItemCommand(params));
+      response.body = JSON.stringify({
+          message: 'Successfully updated employee details.',
+          updateResult,
+      });
+  } catch (e) {
+      console.error(e);
+      response.statusCode = 500;
+      response.body = JSON.stringify({
+          message: 'Failed to update employee details.',
+          errorMsg: e.message,
+          errorStack: e.stack,
+      });
+  }
+  return response;
+};
 
 
 module.exports = {
